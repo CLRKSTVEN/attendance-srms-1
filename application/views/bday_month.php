6800b42520e29db8479eeef7ee565e148c490082
Clark Steven <edongclarksteven@gmail.com>
2025-10-23 17:00:00 +0800

    asd

--- application/views/bday_month.php
+++ application/views/bday_month.php
@@ -8,7 +8,7 @@
     <meta content="Responsive bootstrap 4 admin template" name="description" />
     <meta content="Coderthemes" name="author" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <link rel="shortcut icon" href="<?= base_url(); ?>assets/images/favicon.ico">
+    <link rel="shortcut icon" href="<?= base_url(); ?>upload\banners\logo1.png">
     <link href="<?= base_url(); ?>assets/libs/sweetalert2/sweetalert2.min.css" rel="stylesheet" type="text/css" />
 
     <!-- App css -->
